bea23e82c04c7c6b6450af1bbd0c9d27f5a8e87e
GabrielleSaraiva11TIA <161879307+GabrielleSaraiva11TIA@users.noreply.github.com>
2025-12-02 19:04:42 -0300

    Segundo commit

--- backend-teste-main/src/index.js
+++ backend-teste-main/src/index.js
@@ -11,20 +11,34 @@ app.use(express.json())
 app.post("/cadastrar", (request, response) => {
     const { name, email, age, password } = request.body.user
 
-    //cadastrar o usuário no banco de dados
-    const insertCommand = `
-        INSERT INTO edutec_malgl(name, email, age, password)
-        VALUES(?, ?, ?, ?)
-    `
-    database.query(insertCommand, [name, email, age, password], (error) => {
+    // 1. Verificar se o e-mail já está cadastrado
+    const checkEmailQuery = "SELECT email FROM edutec_malgl WHERE email = ?"
+
+    database.query(checkEmailQuery, [email], (error, results) => {
         if(error) {
             console.log(error)
-            return
+            return response.status(500).json({ message: "Erro no servidor." })
         }
 
-            response.status(201).json({message: "Usuário cadastrado com sucesso!"})    })
+        // SE O E-MAIL EXISTE → cancelar cadastro
+        if(results.length > 0) {
+            return response.status(400).json({ message: "Este e-mail já está cadastrado!" })
+        }
 
-    
+        // 2. SE NÃO EXISTE → cadastrar o usuário
+        const insertCommand = `
+            INSERT INTO edutec_malgl(name, email, age, password)
+            VALUES(?, ?, ?, ?)
+        `
+        database.query(insertCommand, [name, email, age, password], (error) => {
+            if(error) {
+                console.log(error)
+                return response.status(500).json({ message: "Erro ao cadastrar usuário." })
+            }
+
+            return response.status(201).json({ message: "Usuário cadastrado com sucesso!" })
+        })
+    })
 })
 
 app.listen(port, () => {
